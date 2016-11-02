bf2f697de4a53509b60314d8686df63fad90ff03
[luizvicenteps] <[luizvicenteps@gmail.com]>
2016-11-02 10:32:15 -0400

    Atualizacao da classe de conexao e .gitignore

--- classe/conexao.php
+++ classe/conexao.php
@@ -5,12 +5,12 @@ $userbd = "root";
 $passbd = "";
 $bd = "cadcli";
 
-$mysqli = new mysqli($host, $userbd, $passbd, $bd);
+/*$mysqli = new mysqli($host, $userbd, $passbd, $bd);
 
 $con = mysql_connect("$host", "$userbd", "$passbd") or die ("Sem conexão com o servidor");
 $select = mysql_select_db("cadcli") or die("Sem acesso ao DB, Entre em contato com o Administrador, gilson_sales@bytecode.com.br");
 #conexao PDO
-
+*/
 try
 {
 $PDO = new PDO( 'mysql:host=' . $host . ';dbname=' . $bd, $userbd, $passbd );
